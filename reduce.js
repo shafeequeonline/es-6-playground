1ea1b4aa7688a0c6a84dbb9daf0d930ea67d2d90
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-01-22 11:37:56 +0530

    es6 reduce function example

--- reduce.js
+++ reduce.js
@@ -16,3 +16,45 @@ sum = numbers.reduce((sum, number) => {
 }, 0)
 
 console.log(sum);
+
+var myMarks = [35, 36, 45, 50];
+
+var myTotal = myMarks.reduce((total, mark) => { return total + mark}, 0)
+
+console.log(myTotal);
+
+var primaryColors = [
+    { color: 'red' },
+    { color: 'green' },
+    { color: 'blue' },
+]
+
+var colors = []
+
+// Map method
+primaryColors.map((color) => {
+    return colors.push(color.color)
+})
+
+// reduce way
+primaryColors.reduce((prev, color) => {
+    prev.push(color.color);
+    console.log(prev);
+    return prev;
+}, []);
+console.log(colors);
+
+// balanced or unbanaced
+
+function isBalanced(string) {
+    const balance = string.split("").reduce((sum, paren) => {
+        if (sum < 0 ) { return sum; }
+        if(paren === "(") { return ++sum; }
+        if(paren === ")") { return --sum; }
+        
+        return sum;
+    }, 0)
+    return balance === 0;
+    
+}
+console.log(isBalanced("())"))
