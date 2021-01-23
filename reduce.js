3eb6bfda2c56abb95c0c8c939c3b56abf262e70d
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-01-23 11:54:41 +0530

    es6 reduce function test task

--- reduce.js
+++ reduce.js
@@ -58,3 +58,74 @@ function isBalanced(string) {
     
 }
 console.log(isBalanced("())"))
+
+
+let shafeequeMarks = [
+    { name : 'IT', mark: 50, gracemark: [20, 25, 15] },
+    { name : 'Social', mark: 35 },
+    { name : 'Sience', mark: 37 }
+]
+
+
+let shafeequeMarksTotal = 0;
+
+let myItGrace = shafeequeMarks[0].gracemark.reduce((a,b) => {return a+b});
+console.log(myItGrace);
+
+shafeequeMarks.reduce((accumulator, shafeeque) => {
+    // console.log(shafeeque.gracemark.reduce((a,b) => { return a + b }, 0));
+    return shafeequeMarksTotal = accumulator + shafeeque.mark;
+}, 0);
+
+console.log(shafeequeMarksTotal);
+
+
+// Ecersice 1
+// calculate distance
+var trips = [{ distance: 34 }, { distance: 12 } , { distance: 1 }];
+
+var totalDistance = trips.reduce((accumulator, trip) => { return accumulator + trip.distance}, 0);
+
+console.log("totalDistance " + totalDistance);
+
+// Excersice 2
+
+var desks = [
+    { type: 'sitting' },
+    { type: 'standing' },
+    { type: 'sitting' },
+    { type: 'sitting' },
+    { type: 'standing' }
+];
+  
+// their solution
+// var deskTypes = desks.reduce(function(previous, desk) {
+//     if(desk.type === 'sitting') { ++previous.sitting }
+//     if(desk.type === 'standing') { ++previous.standing }
+//     return previous;
+// }, { sitting: 0, standing: 0 });
+
+var deskTypes = desks.reduce(function(prev, desk) {
+    desk.type === 'sitting' ? prev.sitting++ : prev.standing++;
+    return prev
+}, { sitting: 0, standing: 0 });
+
+console.log(deskTypes);
+
+
+// remove duplocates from array
+
+var mynumbers = [1,1,2,3,4,4, 5, 5];
+
+function unique(array) {
+    return array.reduce((previouseData, current) => {
+       if(!previouseData.find((prevItem) => {
+           return prevItem === current
+       })) {
+            previouseData.push(current)
+       }
+       return previouseData;
+    }, [])
+}
+
+console.log(unique(mynumbers));
