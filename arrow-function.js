d422b586950be3162207e0339581fa107b64f2df
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-01-24 09:19:59 +0530

    es6 arrow function another example

--- arrow-function.js
+++ arrow-function.js
@@ -12,7 +12,21 @@ const add = (a,b) => {
 }
 console.log(add(10,20));
 
-// even better, if we have one return key we can remove the curly brackets too
+// even better, if we have one return key we can remove the return keyword and curly brackets too
+// if only sigle argument we can remove brackets too
 
 const addition = (a,b) => a+b;
 console.log(addition(100,200));
+
+
+// ES5
+function double(number) {
+    return number * 2;
+}
+
+// ES6
+// removed the brackets since we have only one argument 
+// removed retund and curly brackets sine we have only 1 return statement
+const doubleNumber = number => number * 2;
+
+console.log(doubleNumber(4));
