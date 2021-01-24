1e6b38b4db10b96fb3f3f6ed06356e52bce32e2b
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-01-24 09:01:38 +0530

    es6 template literals examples

--- template-string.js
+++ template-string.js
@@ -5,3 +5,27 @@ function getMessage() {
 }
 
 console.log(getMessage());
+
+// ES5 method
+function doubleMessage(number) {
+    return "Your number doubled is " + (2 * number);
+}
+
+// ES6 Method
+function doubleMessage(number) {
+    return `Your number doubled is ${number * 2}`;
+}
+
+console.log(doubleMessage(2));
+
+// ES5 method
+function fullName(firstName, lastName) {
+    return firstName + lastName;
+}
+
+// ES6 Method
+function fullName(firstName, lastName) {
+    return `${firstName} ${lastName}` 
+}
+
+console.log(fullName('Mohamed', 'Shafeeque'));
