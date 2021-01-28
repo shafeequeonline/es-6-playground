e3466e320b3c82b8c50ca513329341f39badbfb1
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-01-28 09:46:15 +0530

    es6 object literal example

--- object-literals.js
+++ object-literals.js
@@ -12,10 +12,39 @@ function createBookShop(inventory) {
 
 const inventory = [
     { title: 'Harry Potter', price: 10 },
-    { title: 'Eloquent Javascript', price: 15 },
+    { title: 'Eloquent Javascript', price: 15 }
 ]
-
 const bookShop = createBookShop(inventory);
 
+console.log(bookShop);
 console.log(bookShop.inventoryValue());
 console.log(bookShop.priceForTitle('Harry Potter'));
+
+
+
+function createMyBookShop(inventory) {
+    return {
+        inventory: inventory,
+        inventoryValue : function() {
+            return this.inventory.reduce((total, book) => total + book.price, 0);
+        },
+        priceForTitle : function(title) {
+            return this.inventory.find(book => book.title === title).price;
+        },
+        titleForPrice: function(price) {
+            return this.inventory.find( book => book.price === price).title;
+        }
+    }
+}
+
+const myInventory = [
+    { title: 'Harry Potter', price: 10 },
+    { title: 'Eloquent Javascript', price: 15 },
+    { title: 'Pragmatic Programmer', price: 50 },
+]
+const myBookShop = createMyBookShop(myInventory);
+
+console.log(myBookShop);
+console.log(myBookShop.inventoryValue());
+console.log(myBookShop.priceForTitle("Harry Potter"));
+console.log(myBookShop.titleForPrice(10));
