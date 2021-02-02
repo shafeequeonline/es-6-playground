aaad5435a641896f7650d983d3fd37ba67576549
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-02 07:30:23 +0530

    es6 default function arguments

---
+++ function-arguments.js
@@ -0,0 +1,15 @@
+function User(id) {
+    this.id = id;
+}
+
+function generateId() {
+    return Math.random() * 99999;
+}
+
+function createAdminUser(user = new User(generateId())) {
+    user.admin = true;
+
+    return user;
+}
+const user = new User(generateId())
+console.log(createAdminUser(user));
