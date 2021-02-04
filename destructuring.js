ee1d379afbcbc38d3a57c580727976716b53df12
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-04 09:41:55 +0530

    es6 destructuring method with object

--- destructuring.js
+++ destructuring.js
@@ -26,3 +26,25 @@ const place = {
 const { name, city, state } = place;
 
 console.log(name, city, state);
+
+// another example
+// ES5
+var savedFile = {
+    extension: 'jpg',
+    name: 'repost',
+    size: 140
+}
+
+function fileSummery(file) {
+    return `The file ${file.name}.${file.extension} is of size ${file.size} MB`;
+}
+
+console.log(fileSummery(savedFile));
+
+// ES6
+// in the first argument we selected the main object, in the second argument we selcted second argument when we called the function
+function fileSummeryES6( { name, extension, size }, { fileSizeType } ) {
+    return `The file ${name}.${extension} is of size ${size} ${fileSizeType}`;
+}
+
+console.log(fileSummeryES6(savedFile, { fileSizeType: 'KB'}));
