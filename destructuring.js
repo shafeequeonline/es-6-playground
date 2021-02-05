1388304d7824bd2f73c96fc9a55c16246558e583
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-05 16:21:27 +0530

    es6 destructuring in an array and object complex method

--- destructuring.js
+++ destructuring.js
@@ -103,6 +103,7 @@ const user = {
     city: 'Malappuram'
 }
 
+
 // used de structuring of object here. Now we don't have to worry about the order of arguments
 function mySignUp( { dateOfBirth, username, password, email, city }) {
     return username + ' ' + password + ' ' + email + ' ' + dateOfBirth + ' ' + city;
