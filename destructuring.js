17e39cdeb9e0405c63eafc4334c1bee643f48514
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-07 12:17:33 +0530

    es6 destructure examlpe

--- destructuring.js
+++ destructuring.js
@@ -145,3 +145,17 @@ const myChart = points.map(([x, y]) => {
 })
 
 console.log(myChart);
+
+
+// example
+const classes = [
+    [ 'Chemistry', '9AM', 'Mr. Darnick' ],
+    [ 'Physics', '10:15AM', 'Mrs. Lithun'],
+    [ 'Math', '11:30AM', 'Mrs. Vitalis' ]
+  ];
+  
+  const classesAsObject = classes.map(([subject, time, teacher]) => {
+      return { subject, time, teacher }
+  });
+
+  console.log(classesAsObject);
