4fafea5335f60693eaf4773d2e4b9a34a95f0d75
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-08 08:19:45 +0530

    es6 classes: example

--- classes.js
+++ classes.js
@@ -8,3 +8,19 @@ Car.prototype.drive = function() {
 
 const myCar = new Car({ title: 'Focus' })
 console.log(myCar, myCar.drive());
+
+
+function Toyota(options) {
+    Car.call(this, options)
+    this.color = options.color;
+}
+
+Toyota.prototype = Object.create(Car.prototype)
+Toyota.prototype.constructor = Toyota;
+Toyota.prototype.honk = function() {
+    return 'beep'
+}
+
+const myToyota = new Toyota({ color: 'red', title: 'Liva' });
+
+console.log(myToyota, myToyota.honk(), myToyota.drive());
