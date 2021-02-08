29c79569c17a1645c80c4d07e4fb379004073a33
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-08 09:52:32 +0530

    es6 classes and costructor excercise

--- classes.js
+++ classes.js
@@ -70,3 +70,28 @@ console.log(myNewCar, myNewCar.drive());
 
 const myToyotaCar = new NewToyota({color: 'dual tone, red and black', title: 'Liva'});
 console.log(myToyotaCar, myToyotaCar.honk(), myToyotaCar.drive());
+
+
+// Excercise
+class Monster {
+    constructor(options) {
+      this.health = 100;
+      this.name = options.name;
+    }
+}
+  
+class Snake extends Monster {
+    constructor(options) {
+        super(options)
+    }
+    
+    bite(snake) {
+        return snake.health -= 10;
+    }
+}
+
+const snake1 = new Snake({ name: 'Fred' });
+const snake2 = new Snake({ name: 'Willy' });
+snake1.bite(snake2);
+console.log(snake1);
+console.log(snake2);
