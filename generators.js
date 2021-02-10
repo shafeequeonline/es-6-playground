6fc94b2f8723080d9f5caff4a6eb87d368c32780
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-10 08:27:38 +0530

    es6 for of another example

--- generators.js
+++ generators.js
@@ -14,3 +14,31 @@ for(let number of numbers) {
     total += number
 }
 console.log(total);
+
+const lukeSkywalker = 'Luke Skywalker'
+
+// old approach
+const obj = {
+    lukeSkywalker : lukeSkywalker
+}
+
+// es6 approach
+const myObj = {
+    lukeSkywalker
+}
+
+console.log(obj, myObj);
+
+const marks = [
+    { subject: 'Science', mark: 79 },
+    { subject: 'IT', mark: 85 },
+    { subject: 'GK', mark: 75 }
+]
+
+let totalMark = 0;
+
+for(let mark of marks) {
+    totalMark += mark.mark
+}
+
+console.log(totalMark);
