8811bc5fea8673152ba8f80622561f637eecf20d
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-11 11:56:21 +0530

    es6 GENERATORS multiple generators combination way

---
+++ generators-extended.js
@@ -0,0 +1,33 @@
+// real world example
+const testingTeam = {
+    lead: 'Bill',
+    tester: 'Arjun',
+    [Symbol.iterator]: function* (){
+        yield this.lead;
+        yield this.tester;
+    }
+}
+
+const engineeringTeam = {
+    testingTeam,
+    size: 3,
+    department: 'Engineering',
+    lead: 'John',
+    manager: 'Edwin',
+    engineer: 'Shafeeque'
+}
+
+function* TeamIterator(team) {
+    yield team.lead;
+    yield team.manager;
+    yield team.engineer;
+    yield* team.testingTeam;
+}
+
+const myEngineeringTeam = []
+
+for(let name of TeamIterator(engineeringTeam)){
+    myEngineeringTeam.push(name)
+}
+
+console.log(myEngineeringTeam);
