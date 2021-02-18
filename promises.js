9a7640fbe228965bbf104b5646a1689fda02bc9a
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-18 21:50:04 +0530

    ES6: Promise example example

--- promises.js
+++ promises.js
@@ -1,3 +1,10 @@
-// let promise = new Promise(() => {
-
+// let promise = new Promise((resolve, reject) => {
+//     reject();
 // });
+
+// // if resolved then we can use 'then'. If rejected we can use 'catch'
+// // We can add multiple 'then' with chaning
+// promise
+//     .then(() => console.log('I am Done :) '))
+//     .then(() => console.log('I am also completed :) '))
+//     .catch(() => console.log('Oh no!!!'))
