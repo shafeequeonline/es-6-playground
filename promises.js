288a85234e8969d1a3a982634a01db38a33ba2ff
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-18 21:59:23 +0530

    ES6: Promise example with fake async

--- promises.js
+++ promises.js
@@ -1,10 +1,13 @@
-// let promise = new Promise((resolve, reject) => {
-//     reject();
-// });
+let promise = new Promise((resolve, reject) => {
+    // faking async with a set timeout
+    setTimeout(() => {
+        reject();
+    }, 5000)
+});
 
-// // if resolved then we can use 'then'. If rejected we can use 'catch'
-// // We can add multiple 'then' with chaning
-// promise
-//     .then(() => console.log('I am Done :) '))
-//     .then(() => console.log('I am also completed :) '))
-//     .catch(() => console.log('Oh no!!!'))
+// if resolved then we can use 'then'. If rejected we can use 'catch'
+// We can add multiple 'then' with chaning
+promise
+    .then(() => console.log('I am Done :) '))
+    .then(() => console.log('I am also completed :) '))
+    .catch(() => console.log('Oh no!!!'))
