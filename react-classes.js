caa26a70cd63ba46a43041923eb5bf713543e339
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2021-02-21 20:57:16 +0530

    React:Spread and Rest

--- react-classes.js
+++ react-classes.js
@@ -1,4 +1,5 @@
 class Human {
+    // in the latest ES7 codes we can remove constructor and this keyword from where we set a property 
     constructor() {
         this.gender = 'male'
     }
@@ -22,3 +23,29 @@ class Person extends Human{
 const person = new Person();
 person.printMyName()
 person.printGender()
+
+
+// spread operator
+const numbers = [1,2,3]
+const newNumbers = [...numbers, 4, 5, 6]
+
+console.log(newNumbers);
+
+const myPerson = {
+    name: 'Shafeeque'
+}
+
+const newPerson = {
+    ...myPerson,
+    age: 31
+}
+
+console.log(newPerson);
+
+// Rest operaror
+
+const filter = (...args) => {
+    return args.filter( el => el === 1);
+}
+
+console.log(filter(1,2,3,));
